41de4f991ea6013af412ca00fb37656df19642e3
LimpidusCoruscare <chelhwankim@gmail.com>
2024-05-15 16:43:36 +0900

    fix: 운동 카테고리 상세 페이지 수정
    - 운동 카테고리 수정 기능 제거에 따른 수정 기능 해제
    - 영상 업로드를 위한 버튼 추가

--- src/pages/Exercise/ExerciseInfo.js
+++ src/pages/Exercise/ExerciseInfo.js
@@ -75,52 +75,79 @@ function ExerciseInfo({ exerId }) {
   };
 
   // 수정 버튼 클릭 시 실행되는 함수
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+    
+  //   try {
+  //     // PUT 요청을 보내 운동 정보 업데이트
+  //     const response = await axios.put(process.env.REACT_APP_API_URL + `/exercises`, {
+  //       exerciseName: exerciseData.exerciseName,
+  //       newExerciseName: exerciseName,
+  //       newExerciseType: exerciseType,
+  //       newPerKcal: perKcal
+  //     });
+      
+  //     // response 객체와 response.data 객체가 존재하는지 확인
+  //     if (response && response.data) {
+  //       if (response.data.success) {
+  //         if (formData) {
+  //           console.log("?uploadFile: ", formData.get('file'));
+  //           const videoResponse = await axios.post(
+  //             process.env.REACT_APP_API_URL + `/exercises/video/${exerciseName}`,
+  //             formData,
+  //             {
+  //               headers: {
+  //                 'Content-Type': 'multipart/form-data',
+  //               },
+  //             }
+  //           );
+  //           console.log("Video upload response: ", videoResponse);
+  //           // 비디오 업로드 성공 시 추가 작업 수행 (예: 비디오 URL 저장)
+  //         }
+  //         console.log("?response: ", response);
+  //         alert("운동 정보 수정 성공: ", response.data.message);
+  //         window.location.reload();
+  //       } else {
+  //         alert('운동 정보 수정 실패: ' + response.data.message);
+  //       }
+  //     } else {
+  //       // response 객체나 response.data 객체가 없는 경우 처리
+  //       alert('서버 응답 오류: ' + response.data.message);
+  //     }
+  //   } catch (error) {
+  //     // 에러 메시지 출력
+  //     console.log('error: ', error);
+  //     alert('Error updating exercise data:', error.response.data.message);
+  //   }
+  // };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
     try {
-      // PUT 요청을 보내 운동 정보 업데이트
-      const response = await axios.put(process.env.REACT_APP_API_URL + `/exercises`, {
-        exerciseName: exerciseData.exerciseName,
-        newExerciseName: exerciseName,
-        newExerciseType: exerciseType,
-        newPerKcal: perKcal
-      });
-      
-      // response 객체와 response.data 객체가 존재하는지 확인
-      if (response && response.data) {
-        if (response.data.success) {
-          if (formData) {
-            console.log("?uploadFile: ", formData.get('file'));
-            const videoResponse = await axios.post(
-              process.env.REACT_APP_API_URL + `/exercises/video/${exerciseName}`,
-              formData,
-              {
-                headers: {
-                  'Content-Type': 'multipart/form-data',
-                },
-              }
-            );
-            console.log("Video upload response: ", videoResponse);
-            // 비디오 업로드 성공 시 추가 작업 수행 (예: 비디오 URL 저장)
+      if (formData) {
+        console.log("?uploadFile: ", formData.get('file'));
+        const response = await axios.post(
+          process.env.REACT_APP_API_URL + `/exercises/video/${exerciseName}`,
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
           }
-          console.log("?response: ", response);
-          alert("운동 정보 수정 성공: ", response.data.message);
+        );
+        if (response.data.success) {
+          alert("영상 업로드 성공");
+          console.log("Video upload response: ", response);
           window.location.reload();
-        } else {
-          alert('운동 정보 수정 실패: ' + response.data.message);
         }
-      } else {
-        // response 객체나 response.data 객체가 없는 경우 처리
-        alert('서버 응답 오류: ' + response.data.message);
+        // 비디오 업로드 성공 시 추가 작업 수행 (예: 비디오 URL 저장)
       }
     } catch (error) {
       // 에러 메시지 출력
       console.log('error: ', error);
-      alert('Error updating exercise data:', error.response.data.message);
+      alert('영상 업로드 중 에러 발생:', error.response.data.message);
     }
   };
-
 
   // 컴포넌트가 마운트될 때 한 번만 실행
   useEffect(() => {
@@ -176,17 +203,17 @@ function ExerciseInfo({ exerId }) {
   }
 
   // 숫자만 입력 가능하도록 하는 함수
-  const handleChange = (e, setValue, setError) => {
-    const inputValue = e.target.value;
-    const isNumber = /^\d+$/.test(inputValue);
+  // const handleChange = (e, setValue, setError) => {
+  //   const inputValue = e.target.value;
+  //   const isNumber = /^\d+$/.test(inputValue);
 
-    if (!isNumber && inputValue !== '') {
-      setError('숫자(소수점 *제외*)만 입력 가능합니다.');
-    } else {
-      setError(null);
-      setValue(inputValue);
-    }
-  };
+  //   if (!isNumber && inputValue !== '') {
+  //     setError('숫자(소수점 *제외*)만 입력 가능합니다.');
+  //   } else {
+  //     setError(null);
+  //     setValue(inputValue);
+  //   }
+  // };
 
   return (
     <Container className="mt-3">
@@ -199,8 +226,12 @@ function ExerciseInfo({ exerId }) {
               </Button>}
             {videoUrl && <video src={videoUrl} controls style={{ width: '100%', height: '100%', paddingBottom: '50px' }} />}
             {showUploadBox && <div style={{ border: '1px solid #ccc', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}><UploadBox onFileUpload={handleFileUpload} extension={'mp4'} /></div>}
+            {formData && <Button variant="primary" onClick={handleSubmit} style={{fontWeight: "bold", position: "absolute", bottom: "0", right: "0"}} >
+              <span className="material-symbols-outlined" style={{verticalAlign: "middle"}}>upload</span>
+              <span style={{verticalAlign: "middle"}}> 영상 업로드</span>
+            </Button>}
           </div>
-          <Form onSubmit={handleSubmit}>
+          <Form>
             <Form.Group as={Row}>
               <Form.Label column sm="3">
                 <span className="material-symbols-outlined" style={{ verticalAlign: 'middle', marginRight: '5px', fontVariationSettings: "'FILL' 1" }}>format_list_numbered</span>
@@ -216,7 +247,7 @@ function ExerciseInfo({ exerId }) {
                 <span style={{verticalAlign: "middle"}}> 운동 이름</span>
               </Form.Label>
               <Col sm="9">
-                <Form.Control value={exerciseName || ''} onChange={(e) => setExerciseName(e.target.value)} />
+                <Form.Control value={exerciseName || ''} disabled />
               </Col>
             </Form.Group>
             <Form.Group as={Row}>
@@ -225,8 +256,8 @@ function ExerciseInfo({ exerId }) {
                 <span style={{verticalAlign: "middle"}}> 소모 칼로리</span>
               </Form.Label>
               <Col sm="9">
-                <Form.Control value={perKcal || ''} onChange={(e) => handleChange(e, setPerKcal, setError)} isInvalid={!!error} />
-                <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+                <Form.Control value={perKcal || ''} disabled />
+                {/* <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback> */}
               </Col>
             </Form.Group>
             <Form.Group as={Row}>
@@ -240,10 +271,11 @@ function ExerciseInfo({ exerId }) {
             </Form.Group>
             <Stack direction="horizontal">
               <ButtonGroup className="pt-2 ms-auto">
-                <Button variant="primary" type="submit" style={{fontWeight: "bold"}}>
+                {/* <Button variant="primary" type="submit" style={{fontWeight: "bold"}}>
                   <span className="material-symbols-outlined" style={{verticalAlign: "middle"}}>edit</span>
                   <span style={{verticalAlign: "middle"}}> 운동 수정</span>
-                </Button>
+                </Button> */}
+                {/* 운동 수정은 DB 무결성에 악영향을 주므로 deprecated */}
                 <Button variant="danger" onClick={deleteClick} style={{fontWeight: "bold"}}>
                   <span className="material-symbols-outlined" style={{verticalAlign: "middle"}}>delete</span>
                   <span style={{verticalAlign: "middle"}}> 운동 삭제</span>
